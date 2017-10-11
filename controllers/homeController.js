7ab34dbe7ed59e99a29fe5fec7bc5cf44ab1ccab
Elan <dongxusheng@live.cn>
2017-10-11 10:38:58 +0800

    更新snowf版本

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,23 @@
-var action = require('snowframework-action');
+var mvc = require('snowframework-mvc');
 
-const homeController = {    
-    indexAction: function (req) {                
-        return action.view();
-    },
-    aboutAction: function (req) {
-        return action.view();
-    },
-    helloAction: function (req) {
-        return action.content('hello world', action.contentType.html);
+const homeController = {
+    indexAction: function (req) {
+        var viewModel = {
+            //render by server
+            viewbag: {
+                title: 'SnowFramework',
+                topic: 'Hello Snowframework!',
+                content: 'A Simple nodeJS Web Framework, these render by server !'
+            },
+            //render by client
+            prop:{
+                content:'hello vue example!',
+                alert:'these render by client'
+            }
+        };
+        
+        return mvc.action.view(viewModel);
     }
-};
+}
 
 module.exports = homeController;
